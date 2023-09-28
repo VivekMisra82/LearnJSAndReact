da890f04788c65e5c7a5a23f437a6b4d899bc389
Vivek Krishna Misra <V.misra@ksolves.com>
2023-09-28 14:59:34 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,12 @@
+function mathop(){
+    var a=50;
+    var b=2;
+    document.write("a+b=",(a+b),"<br>");
+    document.write("a-b=",(a-b),"<br>");
+    document.write("a*b=",(a*b),"<br>");
+    document.write("a/b=",(a/b));
+    }
+
+    function welcome(){
+        document.write("welcome");
+    }
